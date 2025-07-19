6b74d879f9b8670729b13fe8d0302433f83ba864
GUAIKATTO <ssdgk233@gmail.com>
2025-07-19 12:02:09 +0800

    初始化vue项目

---
+++ nankai-vag/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h1>Welcome to My Vue App</h1>
+    <p>This is a simple Vue.js application.</p>
+  </div>
+</template>
+
+<style>
+#app {
+  background-color:#000;
+  min-height: 100vh;
+  width: 100vm;
+}
+</style>
